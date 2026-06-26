55d968ace84ba297ea98c46c3de473321d68716e
Adriano160406 <adrianomonteirorolim@gmail.com>
2026-06-26 16:20:25 -0400

    .

--- ui/menuUsuarios.py
+++ ui/menuUsuarios.py
@@ -2,7 +2,7 @@ from models.aluno import Aluno
 from models.personal import Personal
 from models.execeptions import CPFInvalidoError, ValorInvalidoError
 
-# Mudamos os parâmetros para receber as camadas de banco/serviço
+
 def menu_usuarios(aluno_service, personal_repository):
     while True:
         print("\n--- GESTAO DE USUARIOS ---")
@@ -23,8 +23,7 @@ def menu_usuarios(aluno_service, personal_repository):
                 altura = float(input("Altura (m): "))
                 
                 aluno = Aluno(nome, cpf, email, telefone, peso, altura)
-                
-                # --- AQUI ENTRA A SUA REGRA DE NEGÓCIO ---
+            
                 aluno_service.cadastrar_aluno(aluno)
                 print(f"Sucesso: Aluno {aluno.nome} cadastrado!")
                 
@@ -33,7 +32,7 @@ def menu_usuarios(aluno_service, personal_repository):
             except ValueError:
                 print("Erro: insira valores numericos validos.")
             except Exception as e:
-                # Captura o erro se o CPF for duplicado na regra de negócio
+                
                 print(f"Erro de Sistema: {e}")
                 
         elif opcao == '2':
@@ -48,7 +47,7 @@ def menu_usuarios(aluno_service, personal_repository):
                 
                 personal = Personal(nome, cpf, email, telefone, cref, especialidade)
                 
-                # Salva o personal no banco usando o repositório do Eduardo
+              
                 personal_repository.salvar(personal)
                 print(f"Sucesso: Personal {personal.nome} cadastrado!")
             except CPFInvalidoError as e:
@@ -60,7 +59,7 @@ def menu_usuarios(aluno_service, personal_repository):
                 
         elif opcao == '3':
             print("\n--- Todos os Usuarios Cadastrados ---")
-            # Busca os dados atualizados vindos do banco de dados
+        
             lista_alunos = aluno_service.listar_alunos()
             lista_personais = personal_repository.listar_todos()
             
@@ -68,7 +67,7 @@ def menu_usuarios(aluno_service, personal_repository):
             if not todos:
                 print("Nenhum usuario cadastrado.")
             for u in todos:
-                print(u.exibir_perfil()) # Executa o comportamento polimorfico
+                print(u.exibir_perfil()) 
                 
         elif opcao == '4':
             break
