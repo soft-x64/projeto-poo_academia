6d25d80b2e6860c12d5d4e0d80b92ff4691d6a10
Eduardo <eduardotundis242@gmail.com>
2026-06-26 18:36:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 import sys
 from ui.menu_principal import exibir_menu_principal
+from database.services import AlunoService, InstrutorRepository
 
 def main():
-    # Listas em memória que simulam as tabelas do banco de dados temporariamente
-    lista_alunos = []
-    lista_personais = []
-    lista_aparelhos = []
-    lista_exercicios = []
-    lista_avaliacoes = []
-    lista_fichas = []
+    # Cria os objetos que o Júlio espera receber nos menus
+    aluno_service = AlunoService()
+    instrutor_repository = InstrutorRepository()
 
     try:
-        exibir_menu_principal(
-            lista_alunos, 
-            lista_personais, 
-            lista_aparelhos, 
-            lista_exercicios, 
-            lista_avaliacoes, 
-            lista_fichas
-        )
+        # Passa os serviços para o menu principal que agora está dentro da pasta ui
+        exibir_menu_principal(aluno_service, instrutor_repository)
     except KeyboardInterrupt:
-        print("\nSistema encerrado de forma forcada. Ate logo!")
+        print("\nSistema encerrado. Ate logo!")
         sys.exit(0)
 
 if __name__ == "__main__":
