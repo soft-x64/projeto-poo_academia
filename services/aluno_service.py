4d46ad6a34e61a3f5a090c5faa8f5c140f6e25ef
Adriano160406 <adrianomonteirorolim@gmail.com>
2026-06-26 16:12:20 -0400

    correção codigo

--- services/aluno_service.py
+++ services/aluno_service.py
@@ -2,18 +2,14 @@
 
 class AlunoService:
     def __init__(self, aluno_repository):
-      
         self.aluno_repo = aluno_repository
 
     def cadastrar_aluno(self, aluno):
-    
         aluno_existente = self.aluno_repo.buscar_por_cpf(aluno.cpf)
         
         if aluno_existente:
-           
             raise Exception(f"Regra de Negócio: O CPF {aluno.cpf} já está cadastrado!")
             
- 
         self.aluno_repo.salvar(aluno)
 
     def listar_alunos(self):
