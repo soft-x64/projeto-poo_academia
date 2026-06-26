eb86347c4e194fa82cf7cb0f0d725e0990c50b2b
Eduardo <eduardotundis242@gmail.com>
2026-06-26 18:12:03 -0300

    Update conexao.py

--- database/conexao.py
+++ database/conexao.py
@@ -1,24 +1,16 @@
 import psycopg2
 from psycopg2 import OperationalError
 
-# Configurações o banco de dados local do Postgres
-# Da pra alterar os valores abaixo de acordo com as credenciais que nós for usar
-DB_SETTINGS = {
-    "dbname": "trainerx64",
-    "user": "postgres",
-    "password": "sua_senha_aqui",
-    "host": "localhost",
-    "port": "5432"
-}
-
 def get_connection():
-    """
-    Retorna uma conexão ativa com o banco de dados PostgreSQL.
-    Lança uma exceção controlada caso falhe.
-    """
     try:
-        connection = psycopg2.connect(**DB_SETTINGS)
-        return connection
+        conexao = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user="postgres",
+            password="123456",
+            port="5433"
+        )
+        return conexao
     except OperationalError as e:
-        print(f"[ERRO DE CONEXÃO]: Não foi possível conectar ao PostgreSQL. {e}")
-        raise e
+        print(f"Erro ao conectar ao PostgreSQL na porta 5433: {e}")
+        return None
