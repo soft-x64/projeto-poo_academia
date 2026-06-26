227573615b825624f45f13f1c457e6d750e46acc
Adriano160406 <adrianomonteirorolim@gmail.com>
2026-06-26 16:27:31 -0400

    ..

--- services/aluno_service.py
+++ services/aluno_service.py
@@ -1,16 +1,30 @@
 # services/aluno_service.py
+from models.aluno import Aluno
 
 class AlunoService:
     def __init__(self, aluno_repository):
+        """Recebe o AlunoRepository desenvolvido pelo Eduardo."""
         self.aluno_repo = aluno_repository
 
-    def cadastrar_aluno(self, aluno):
-        aluno_existente = self.aluno_repo.buscar_por_cpf(aluno.cpf)
+    def cadastrar_aluno(self, aluno_obj):
         
-        if aluno_existente:
-            raise Exception(f"Regra de Negócio: O CPF {aluno.cpf} já está cadastrado!")
+        registro_banco = self.aluno_repo.buscar_por_cpf(aluno_obj.cpf)
+        
+        if registro_banco is not None:
+   
+            raise Exception(f"Regra de Negócio: O CPF {aluno_obj.cpf} já está cadastrado no banco de dados!")
             
-        self.aluno_repo.salvar(aluno)
+      
+        self.aluno_repo.inserir(aluno_obj)
 
     def listar_alunos(self):
-        return self.aluno_repo.listar_todos()
+        """Retorna os alunos convertidos de tuplas do banco para objetos da classe Aluno."""
+        linhas_banco = self.aluno_repo.listar_todos()
+        lista_objetos = []
+        
+        for linha in linhas_banco:
+            
+            aluno_obj = Aluno(linha[1], linha[2], linha[3], linha[4], 0.0, 0.0)
+            lista_objetos.append(aluno_obj)
+            
+        return lista_objetos
