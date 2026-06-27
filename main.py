9b89f709f5a1f1123cf3beb033a3fdda4328d462
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:31:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-from database import get_connection
-
-# Importando os Repositories
-from repositories.aluno_repository import AlunoRepository
-from repositories.instrutor_repository import InstrutorRepository
-from repositories.aparelho_repository import AparelhoRepository
-from repositories.exercicio_repository import ExercicioRepository
-from repositories.ficha_treino_repository import FichaTreinoRepository
-from repositories.avaliacao_fisica_repository import AvaliacaoFisicaRepository
-
-# Importando os Services
-from services.aluno_service import AlunoService
-from services.instrutor_service import InstrutorService
-from services.aparelho_service import AparelhoService
-from services.exercicio_service import ExercicioService
-from services.ficha_treino_service import FichaTreinoService
-from services.avaliacao_fisica_service import AvaliacaoFisicaService
-
-# Importando a função do menu
-from ui.menu_principal import exibir_menu_principal
+from repositories import AlunoRepository, InstrutorRepository, AparelhoRepository, ExercicioRepository, FichaTreinoRepository, AvaliacaoFisicaRepository
+from services import AlunoService, InstrutorService, AparelhoService, ExercicioService, FichaTreinoService, AvaliacaoFisicaService
+from ui import exibir_menu_principal
 
 def main():
-    # Instanciando Repositories
+    # 1. Instanciamento dos Repositórios (conectam ao banco)
     aluno_repo = AlunoRepository()
     instrutor_repo = InstrutorRepository()
     aparelho_repo = AparelhoRepository()
@@ -28,7 +11,7 @@ def main():
     ficha_repo = FichaTreinoRepository()
     avaliacao_repo = AvaliacaoFisicaRepository()
 
-    # Instanciando Services (Injeção de Dependência)
+    # 2. Instanciamento dos Services (regras de negócio)
     aluno_service = AlunoService(aluno_repo)
     instrutor_service = InstrutorService(instrutor_repo)
     aparelho_service = AparelhoService(aparelho_repo)
@@ -36,7 +19,8 @@ def main():
     ficha_service = FichaTreinoService(ficha_repo)
     avaliacao_service = AvaliacaoFisicaService(avaliacao_repo)
 
-    # Executa o menu passando todos os serviços necessários
+    # 3. Execução do Menu (Injeção de Dependência)
+    # Aqui passamos todos os serviços que a UI precisa para funcionar
     exibir_menu_principal(
         aluno_service, 
         instrutor_service, 
