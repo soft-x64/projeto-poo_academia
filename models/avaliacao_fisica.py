41ba7256e7d17c1b45d0f4968614873667f6fbf9
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:16:05 -0300

    Update avaliacao_fisica.py

--- models/avaliacao_fisica.py
+++ models/avaliacao_fisica.py
@@ -1,22 +1,27 @@
-from datetime import date
-from models.execeptions import ValorInvalidoError
-
 class AvaliacaoFisica:
-    def __init__(self, aluno, data: date, percentual_gordura: float, observacoes: str = ""):
-        self._aluno = aluno
-        self._data = data
-        self.percentual_gordura = percentual_gordura
-        self._observacoes = observacoes
+    def __init__(self, aluno_id, peso, altura, data_avaliacao, id=None):
+        self._id = id
+        self._aluno_id = aluno_id
+        self._peso = peso
+        self._altura = altura
+        self._data_avaliacao = data_avaliacao
 
     @property
-    def percentual_gordura(self):
-        return self._percentual_gordura
+    def id(self):
+        return self._id
 
-    @percentual_gordura.setter
-    def percentual_gordura(self, valor):
-        if not (0 < valor < 100):
-             raise ValorInvalidoError("Percentual de gordura deve estar entre 0 e 100")
-        self._percentual_gordura = valor
+    @property
+    def aluno_id(self):
+        return self._aluno_id
 
-    def __str__(self):
-        return f"Avaliação de {self._aluno.nome} em {self._data}: {self.percentual_gordura}% gordura"
+    @property
+    def peso(self):
+        return self._peso
+
+    @property
+    def altura(self):
+        return self._altura
+
+    @property
+    def data_avaliacao(self):
+        return self._data_avaliacao
