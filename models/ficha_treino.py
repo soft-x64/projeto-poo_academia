0ef3770c9af8c0d36984f7f425d7a67f42c46333
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:16:40 -0300

    Update ficha_treino.py

--- models/ficha_treino.py
+++ models/ficha_treino.py
@@ -1,33 +1,22 @@
-from datetime import date
-from models.execeptions import FichaSemExercicioError
+class FichaTreino:
+    def __init__(self, aluno_id, instrutor_id, descricao, id=None):
+        self._id = id
+        self._aluno_id = aluno_id
+        self._instrutor_id = instrutor_id
+        self._descricao = descricao
 
-class ItemFicha:
-    def __init__(self, exercicio, series: int, repeticoes: int, carga: float):
-        self.exercicio = exercicio
-        self.series = series
-        self.repeticoes = repeticoes
-        self.carga = carga
-    
-    def __str__(self):
-        return f"{self.exercicio.nome}: {self.series}x{self.repeticoes} ({self.carga}kg)"
+    @property
+    def id(self):
+        return self._id
 
+    @property
+    def aluno_id(self):
+        return self._aluno_id
 
-class FichaTreino: # Corrigida a indentacao (agora esta fora da outra classe)
-    def __init__(self, aluno, instrutor, data_criacao: date = None):
-        self.aluno = aluno
-        self.instrutor = instrutor
-        self.data_criacao = data_criacao or date.today() # Corrigido para .today()
-        self._itens = [] # Inicializada a lista de itens que faltava
+    @property
+    def instrutor_id(self):
+        return self._instrutor_id
 
-    def adicionar_item(self, item: ItemFicha):
-        self._itens.append(item)
-    
-    def finalizar(self):
-        if not self._itens:
-            raise FichaSemExercicioError("Ficha de treino precisa de pelo menos um exercicio")
-
-    def __str__(self):
-        # Removidos os underlines das variaveis aluno e instrutor para bater com o __init__
-        linhas = [f"Ficha de {self.aluno.nome} (Instrutor: {self.instrutor.nome})"]
-        linhas += [f"  - {item}" for item in self._itens]
-        return "\n".join(linhas)
+    @property
+    def descricao(self):
+        return self._descricao
