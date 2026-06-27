1b9a0d54d5e9a14f18d1257d1929e96474db514d
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:21:31 -0300

    Update instrutor_repository.py

--- repositories/instrutor_repository.py
+++ repositories/instrutor_repository.py
@@ -1,16 +1,22 @@
-from database.connection import get_connection
+from database import get_connection
 
 class InstrutorRepository:
     def inserir(self, instrutor_obj):
         sql = """
-            INSERT INTO instrutor (nome, cpf, email, telefone, cref) 
+            INSERT INTO instrutor (nome, cpf, email, telefone, especialidade) 
             VALUES (%s, %s, %s, %s, %s) RETURNING id;
         """
         conn = None
         try:
             conn = get_connection()
             cursor = conn.cursor()
-            cursor.execute(sql, (instrutor_obj.nome, instrutor_obj.cpf, instrutor_obj.email, instrutor_obj.telefone, instrutor_obj.cref))
+            cursor.execute(sql, (
+                instrutor_obj.nome, 
+                instrutor_obj.cpf, 
+                instrutor_obj.email, 
+                instrutor_obj.telefone, 
+                instrutor_obj.especialidade
+            ))
             id_inserido = cursor.fetchone()[0]
             conn.commit()
             cursor.close()
@@ -23,7 +29,7 @@ class InstrutorRepository:
             if conn: conn.close()
 
     def listar_todos(self):
-        sql = "SELECT id, nome, cpf, email, telefone, cref FROM instrutor ORDER BY nome;"
+        sql = "SELECT id, nome, cpf, email, telefone, especialidade FROM instrutor ORDER BY nome;"
         conn = None
         try:
             conn = get_connection()
@@ -35,5 +41,21 @@ class InstrutorRepository:
         except Exception as e:
             print(f"[Erro InstrutorRepository.listar_todos]: {e}")
             return []
+        finally:
+            if conn: conn.close()
+
+    def buscar_por_cpf(self, cpf):
+        sql = "SELECT id FROM instrutor WHERE cpf = %s;"
+        conn = None
+        try:
+            conn = get_connection()
+            cursor = conn.cursor()
+            cursor.execute(sql, (cpf,))
+            resultado = cursor.fetchone()
+            cursor.close()
+            return resultado is not None
+        except Exception as e:
+            print(f"[Erro InstrutorRepository.buscar_por_cpf]: {e}")
+            return False
         finally:
             if conn: conn.close()
