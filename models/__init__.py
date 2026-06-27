c239ca31d0c6ed7ff4091fee8b9ccbef76e6ea1c
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:14:44 -0300

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .pessoa import Pessoa
+from .aluno import Aluno
+from .instrutor import Instrutor
+from .aparelho import Aparelho
+from .exercicio import Exercicio
+from .ficha_treino import FichaTreino
+from .avaliacao_fisica import AvaliacaoFisica
