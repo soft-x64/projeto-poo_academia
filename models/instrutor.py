1559718b3e52b193ec42e514f369b57bfc8c61ad
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:16:54 -0300

    Update instrutor.py

--- models/instrutor.py
+++ models/instrutor.py
@@ -1,21 +1,36 @@
-from models.pessoa import Pessoa
+from .pessoa import Pessoa
 
-class Personal(Pessoa):
-    def __init__(self, nome, cpf, email, telefone, cref: str, especialidade: str):
+class Instrutor(Pessoa):
+    def __init__(self, nome, cpf, email, telefone, especialidade, id=None):
+        # Chama o construtor da classe base Pessoa
         super().__init__(nome, cpf, email, telefone)
-        self.cref = cref
-        self.especialidade = especialidade
-    
+        self._id = id
+        self._especialidade = especialidade
+
+    def exibir_perfil(self):
+        return f"[INSTRUTOR] {self._nome} | Especialidade: {self._especialidade}"
+
+    # Getters para acesso via @property (mesmo padrão do Aluno)
     @property
-    def cref(self):
-        return self._cref
-    
+    def id(self):
+        return self._id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def telefone(self):
+        return self._telefone
+
     @property
     def especialidade(self):
         return self._especialidade
-    
-    def exibir_perfil(self) -> str:
-        return f"[PERSONAL]: {self.nome} | CREF: {self.cref} | Especialidade: {self.especialidade}"
-    
-    def __str__(self):
-        return self.exibir_perfil()
