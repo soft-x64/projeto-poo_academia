235cc03f379f428d679221e1de50535c54dca1b0
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:22:52 -0300

    Update aluno_service.py

--- services/aluno_service.py
+++ services/aluno_service.py
@@ -1,23 +1,18 @@
-from models.aluno import Aluno
 class AlunoService:
-    def __init__(self, aluno_repository):
-    
-        self.aluno_repo = aluno_repository
+    def __init__(self, repository):
+        self.repository = repository
 
-    def cadastrar_aluno(self, aluno_obj: Aluno):
-        
-        registro_existente = self.aluno_repo.buscar_por_cpf(aluno_obj.cpf)
-        
-        if registro_existente:
-       
-            raise Exception(f"Regra de Negocio: O CPF '{aluno_obj.cpf}' ja esta cadastrado no sistema!")
-        id_gerado = self.aluno_repo.inserir(aluno_obj)
-        return id_gerado
+    def cadastrar_aluno(self, aluno_obj):
+        if self.repository.buscar_por_cpf(aluno_obj.cpf):
+            print("Erro: CPF já cadastrado!")
+            return None
+        return self.repository.inserir(aluno_obj)
+
+    def atualizar_aluno(self, aluno_obj):
+        return self.repository.atualizar(aluno_obj)
 
     def listar_alunos(self):
-        linhas = self.aluno_repo.listar_todos()
-        alunos_objetos = []
-        for linha in linhas:
-            aluno = Aluno(linha[1], linha[2], linha[3], linha[4], 0.0, 0.0)
-            alunos_objetos.append(aluno)
-        return alunos_objetos
+        return self.repository.listar_todos()
+
+    def remover_aluno(self, id_aluno):
+        return self.repository.excluir(id_aluno)
