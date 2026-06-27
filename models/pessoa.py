225b4b72728c9b1c6ba4e9d48ba77aa3a707658e
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:17:11 -0300

    Update pessoa.py

--- models/pessoa.py
+++ models/pessoa.py
@@ -1,34 +1,12 @@
-from models.execeptions import CPFInvalidoError
 from abc import ABC, abstractmethod
+
 class Pessoa(ABC):
-    def __init__(self,nome: str, cpf: str, email: str, telefone: str):
-        self.nome= nome
-        self.cpf = cpf
-        self.email = email
-        self.telefone = telefone
-    
-    @property
-    def nome(self):
-        return self._nome
-    
-    @nome.setter
-    def nome(self, valor):
-        self._nome = valor if valor else ""
+    def __init__(self, nome, cpf, email, telefone):
+        self._nome = nome
+        self._cpf = cpf
+        self._email = email
+        self._telefone = telefone
 
-    @property
-    def cpf(self):
-        return self._cpf
-    
-    @cpf.setter
-    def cpf(self, valor):
-        if not valor or len(valor) != 11 or not valor.isdigit():
-            raise CPFInvalidoError(f"CPF: invalido: `{valor}")
-        self._cpf = valor
-    
     @abstractmethod
-    def exibir_perfil(self) -> str:
+    def exibir_perfil(self):
         pass
-
-    def __str__(self):
-        return f"`{self.nome} (CPF: `{self._cpf})"
-    
