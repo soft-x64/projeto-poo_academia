278b784561b001ea8b394fda5ec5c38f9034dd70
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:16:24 -0300

    Refactor Exercicio class to include an id attribute

--- models/exercicio.py
+++ models/exercicio.py
@@ -1,17 +1,17 @@
 class Exercicio:
-    def __init__(self, nome: str, grupo_muscular: str):
-        self.nome = nome
-        self.grupo_muscular = grupo_muscular
-    
+    def __init__(self, nome, grupo_muscular, id=None):
+        self._id = id
+        self._nome = nome
+        self._grupo_muscular = grupo_muscular
+
+    @property
+    def id(self):
+        return self._id
+
     @property
     def nome(self):
         return self._nome
-    
+
     @property
     def grupo_muscular(self):
         return self._grupo_muscular
-    
-    def __str__(self):
-        return f"{self.nome} ({self.grupo_muscular})"
-    
-    
