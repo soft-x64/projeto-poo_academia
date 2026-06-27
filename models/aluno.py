7c9614115bd443e4614e8658e9dda49b86f9f1ec
Eduardo <eduardotundis242@gmail.com>
2026-06-27 18:15:02 -0300

    Update aluno.py

--- models/aluno.py
+++ models/aluno.py
@@ -1,34 +1,35 @@
-from models.pessoa import Pessoa
-from models.execeptions import ValorInvalidoError
+from .pessoa import Pessoa
 
 class Aluno(Pessoa):
-    def __init__(self, nome, cpf, email, telefone, peso: float, altura: float):
-        super().__init__(nome, cpf, email,telefone)
-        self.peso = peso
-        self.altura = altura
-    
+    def __init__(self, nome, cpf, email, telefone, peso, id=None):
+        super().__init__(nome, cpf, email, telefone)
+        self._id = id  # ID necessário para operações de atualização e exclusão
+        self._peso = peso
+
+    def exibir_perfil(self):
+        return f"[ALUNO] {self._nome} | Peso: {self._peso}kg"
+
+    # Adicionei os getters caso precise acessar os valores protegidos fora da classe
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @property
+    def cpf(self):
+        return self._cpf
+
+    @property
+    def email(self):
+        return self._email
+
+    @property
+    def telefone(self):
+        return self._telefone
+
     @property
     def peso(self):
         return self._peso
-    
-    @peso.setter
-    def peso(self, valor):
-        if valor <= 0:
-            raise ValorInvalidoError("Peso deve ser maior que zero")
-        self._peso = valor
-
-    @property
-    def altura(self):
-        return self._altura
-    
-    @altura.setter
-    def altura(self, valor):
-        if valor <= 0:
-            raise ValorInvalidoError("Altura deve ser maior que zero")
-        self._altura = valor
-    
-    def exibir_perfil(self) -> str:
-        return f"[ALUNO] {self.nome} | Peso: {self.peso}kg | Altura: {self.altura}m"
-    
-    def __str__(self):
-        return self.exibir_perfil()
