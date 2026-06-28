4fed1dbbe167ad0f7d7e126f9b3da95eb5e05e6b
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:26:28 -0300

    Refactor Aparelho class constructor and properties

--- models/aparelho.py
+++ models/aparelho.py
@@ -1,17 +1,5 @@
 class Aparelho:
-    def __init__(self, nome, tipo, id=None):
-        self._id = id
-        self._nome = nome
-        self._tipo = tipo
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def nome(self):
-        return self._nome
-
-    @property
-    def tipo(self):
-        return self._tipo
+    def __init__(self, id=None, nome=None, grupoMuscular=None):
+        self.id = id
+        self.nome = nome
+        self.grupoMuscular = grupoMuscular
