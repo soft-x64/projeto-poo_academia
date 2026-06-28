2f6454da7d353be4a178bc561ec8c81528fff800
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:26:05 -0300

    Refactor Aluno class to remove inheritance from Pessoa

--- models/aluno.py
+++ models/aluno.py
@@ -1,35 +1,7 @@
-from .pessoa import Pessoa
-
-class Aluno(Pessoa):
-    def __init__(self, nome, cpf, email, telefone, peso, id=None):
-        super().__init__(nome, cpf, email, telefone)
-        self._id = id  # ID necessário para operações de atualização e exclusão
-        self._peso = peso
-
-    def exibir_perfil(self):
-        return f"[ALUNO] {self._nome} | Peso: {self._peso}kg"
-
-    # Adicionei os getters caso precise acessar os valores protegidos fora da classe
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def nome(self):
-        return self._nome
-
-    @property
-    def cpf(self):
-        return self._cpf
-
-    @property
-    def email(self):
-        return self._email
-
-    @property
-    def telefone(self):
-        return self._telefone
-
-    @property
-    def peso(self):
-        return self._peso
+class Aluno:
+    def __init__(self, id=None, nomecompleto=None, email=None, contato=None, objetivo=None):
+        self.id = id
+        self.nomecompleto = nomecompleto
+        self.email = email
+        self.contato = contato
+        self.objetivo = objetivo
