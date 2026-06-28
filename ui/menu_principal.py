7f3aba0be14dd7ae76131b6cfa7ba3e72680c1a1
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:33:26 -0300

    Refactor main menu to use submenus

--- ui/menu_principal.py
+++ ui/menu_principal.py
@@ -1,25 +1,20 @@
+from ui.menu_aluno import sub_menu_aluno
+from ui.menu_instrutor import sub_menu_instrutor
+from ui.menu_aparelho import sub_menu_aparelho
+from ui.menu_exercicio import sub_menu_exercicio
+from ui.menu_ficha import sub_menu_ficha
+from ui.menu_avaliacao_fisica import sub_menu_avaliacao_fisica
 
-def exibir_menu_principal(aluno_service, instrutor_service, aparelho_service, exercicio_service, ficha_service, avaliacao_service):
+def exibir_menu_principal(aluno_s, instrutor_s, aparelho_s, exercicio_s, ficha_s, ficha_ex_s, avaliacao_s):
     while True:
-        print("\n--- SISTEMA DE GERENCIAMENTO DE ACADEMIA ---")
-        print("1. Gerenciar Alunos")
-        print("2. Gerenciar Instrutores")
-        print("3. Gerenciar Aparelhos")
-        print("4. Gerenciar Exercícios")
-        print("5. Gerenciar Fichas de Treino")
-        print("6. Gerenciar Avaliações Físicas")
-        print("0. Sair")
+        print("\n--- SISTEMA DE ACADEMIA ---")
+        print("1. Alunos | 2. Instrutores | 3. Aparelhos | 4. Exercícios | 5. Fichas | 6. Avaliações | 7. Sair")
+        opcao = input("Escolha: ")
         
-        opcao = input("Escolha uma opção: ")
-
-        if opcao == "1":
-            # Exemplo de chamada: gerenciar_alunos(aluno_service)
-            print("Menu de Alunos...")
-        elif opcao == "2":
-            print("Menu de Instrutores...")
-        # ... outros elifs para cada serviço ...
-        elif opcao == "0":
-            print("Saindo do sistema...")
-            break
-        else:
-            print("Opção inválida!")
+        if opcao == "1": sub_menu_aluno(aluno_s)
+        elif opcao == "2": sub_menu_instrutor(instrutor_s)
+        elif opcao == "3": sub_menu_aparelho(aparelho_s)
+        elif opcao == "4": sub_menu_exercicio(exercicio_s)
+        elif opcao == "5": sub_menu_ficha(ficha_s) 
+        elif opcao == "6": sub_menu_avaliacao_fisica(avaliacao_s)
+        elif opcao == "7": break
