8f7fcfd1bbdeaebd414950a7091ec8d2e4008bc6
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:27:57 -0300

    Create item_ficha_treino.py

---
+++ models/item_ficha_treino.py
@@ -0,0 +1,8 @@
+class ItemFichaTreino:
+    def __init__(self, id=None, idFicha=None, idExercicio=None, series=None, repeticoes=None, cargas=None):
+        self.id = id
+        self.idFicha = idFicha
+        self.idExercicio = idExercicio
+        self.series = series
+        self.repeticoes = repeticoes
+        self.cargas = cargas
