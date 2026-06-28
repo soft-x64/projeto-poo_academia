5623e299a6daa47dd7c7aa7269b82ba7ba36608f
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:30:53 -0300

    Initialize services module with service imports

---
+++ services/__init__.py
@@ -0,0 +1,6 @@
+from .aluno_service import AlunoService
+from .instrutor_service import InstrutorService
+from .aparelho_service import AparelhoService
+from .exercicio_service import ExercicioService
+from .ficha_treino_service import FichaTreinoService
+from .avaliacao_fisica_service import AvaliacaoFisicaService
