8f7a6160cd5fae56c3bfdcce775c406915bf7d13
Eduardo <eduardotundis242@gmail.com>
2026-06-28 16:42:16 -0300

    Update avaliacao_fisica_repository.py

--- repositories/avaliacao_fisica_repository.py
+++ repositories/avaliacao_fisica_repository.py
@@ -3,19 +3,18 @@ from models.avaliacao_fisica import AvaliacaoFisica
 
 class AvaliacaoFisicaRepository:
     def __init__(self):
-        self.conn = psycopg2.connect("dbname=academia_poo user=postgres password=123456 host=localhost")
+        self.conn = psycopg2.connect(dbname="academia_poo", user="postgres", password="123456", host="localhost")
 
     def salvar(self, av):
         cursor = self.conn.cursor()
-        sql = """INSERT INTO avaliacao_fisica (alunoid, instrutorid, peso, altura, imc) 
-                 VALUES (%s, %s, %s, %s, %s)"""
-        cursor.execute(sql, (av.alunoId, av.instrutorId, av.peso, av.altura, av.imc))
+        sql = "INSERT INTO avaliacao_fisica (alunoid, instrutorid, peso, altura, imc) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, (av.id_aluno, av.id_instrutor, av.peso, av.altura, av.imc))
         self.conn.commit()
         cursor.close()
 
-    def buscar_por_aluno(self, aluno_id):
+    def buscar_por_aluno(self, id_aluno):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT id, alunoid, instrutorid, data_avaliacao, peso, altura, imc FROM avaliacao_fisica WHERE alunoid = %s", (aluno_id,))
+        cursor.execute("SELECT id, alunoid, instrutorid, data_avaliacao, peso, altura, imc FROM avaliacao_fisica WHERE alunoid = %s", (id_aluno,))
         resultados = cursor.fetchall()
         cursor.close()
-        return [AvaliacaoFisica(r[0], r[1], r[2], r[3], r[4], r[5], r[6]) for r in resultados]
+        return [AvaliacaoFisica(id=r[0], id_aluno=r[1], id_instrutor=r[2], data=r[3], peso=r[4], altura=r[5], imc=r[6]) for r in resultados]
