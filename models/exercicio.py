0a06abfa3cc73dfdd04a7b10940571c301fdb3b6
Eduardo <eduardotundis242@gmail.com>
2026-06-28 16:40:25 -0300

    Update exercicio.py

--- models/exercicio.py
+++ models/exercicio.py
@@ -1,7 +1,6 @@
 class Exercicio:
-    def __init__(self, id=None, nome=None, grupomuscular=None, descricaoaudio=None, idaparelho=None):
+    def __init__(self, id=None, nome=None, grupo_muscular=None, id_aparelho=None):
         self.id = id
         self.nome = nome
-        self.grupomuscular = grupomuscular
-        self.descricaoaudio = descricaoaudio
-        self.idaparelho = idaparelho
+        self.grupo_muscular = grupo_muscular
+        self.id_aparelho = id_aparelho
