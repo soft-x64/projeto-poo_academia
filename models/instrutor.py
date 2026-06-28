2a5294d34e11ec8f3a4e4bf2258310d35e94cf10
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:27:31 -0300

    Refactor Instrutor class structure and attributes
    Refactor Instrutor class to remove inheritance from Pessoa and update attributes.

--- models/instrutor.py
+++ models/instrutor.py
@@ -1,36 +1,8 @@
-from .pessoa import Pessoa
-
-class Instrutor(Pessoa):
-    def __init__(self, nome, cpf, email, telefone, especialidade, id=None):
-        # Chama o construtor da classe base Pessoa
-        super().__init__(nome, cpf, email, telefone)
-        self._id = id
-        self._especialidade = especialidade
-
-    def exibir_perfil(self):
-        return f"[INSTRUTOR] {self._nome} | Especialidade: {self._especialidade}"
-
-    # Getters para acesso via @property (mesmo padrão do Aluno)
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def nome(self):
-        return self._nome
-
-    @property
-    def cpf(self):
-        return self._cpf
-
-    @property
-    def email(self):
-        return self._email
-
-    @property
-    def telefone(self):
-        return self._telefone
-
-    @property
-    def especialidade(self):
-        return self._especialidade
+class Instrutor:
+    def __init__(self, id=None, nomecompleto=None, cpf=None, email=None, telefone=None, especialidade=None):
+        self.id = id
+        self.nomecompleto = nomecompleto
+        self.cpf = cpf
+        self.email = email
+        self.telefone = telefone
+        self.especialidade = especialidade
