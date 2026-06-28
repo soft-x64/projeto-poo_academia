f28faf54139613964f324b9905e1a4c55ddbe422
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:26:47 -0300

    Update avaliacao_fisica.py

--- models/avaliacao_fisica.py
+++ models/avaliacao_fisica.py
@@ -1,27 +1,9 @@
 class AvaliacaoFisica:
-    def __init__(self, aluno_id, peso, altura, data_avaliacao, id=None):
-        self._id = id
-        self._aluno_id = aluno_id
-        self._peso = peso
-        self._altura = altura
-        self._data_avaliacao = data_avaliacao
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def aluno_id(self):
-        return self._aluno_id
-
-    @property
-    def peso(self):
-        return self._peso
-
-    @property
-    def altura(self):
-        return self._altura
-
-    @property
-    def data_avaliacao(self):
-        return self._data_avaliacao
+    def __init__(self, id=None, alunoId=None, instrutorId=None, data=None, peso=None, altura=None, imc=None):
+        self.id = id
+        self.alunoId = alunoId
+        self.instrutorId = instrutorId
+        self.data = data
+        self.peso = peso
+        self.altura = altura
+        self.imc = imc
