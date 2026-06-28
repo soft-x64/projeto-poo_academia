6af6029e62546209221cb8e612390f2f6bc13ec8
Eduardo <eduardotundis242@gmail.com>
2026-06-28 16:42:28 -0300

    Update exercicio_repository.py

--- repositories/exercicio_repository.py
+++ repositories/exercicio_repository.py
@@ -7,20 +7,24 @@ class ExercicioRepository:
 
     def salvar(self, exercicio):
         cursor = self.conn.cursor()
-        sql = "INSERT INTO exercicio (nome, grupomuscular, descricaoaudio, idaparelho) VALUES (%s, %s, %s, %s)"
-        cursor.execute(sql, (exercicio.nome, exercicio.grupomuscular, exercicio.descricaoaudio, exercicio.idaparelho))
+        # O banco espera 'grupomuscular' e 'idaparelho'
+        sql = "INSERT INTO exercicio (nome, grupomuscular, idaparelho) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (exercicio.nome, exercicio.grupo_muscular, exercicio.id_aparelho))
         self.conn.commit()
         cursor.close()
 
     def listar(self):
         cursor = self.conn.cursor()
-        cursor.execute("SELECT id, nome, grupomuscular, descricaoaudio, idaparelho FROM exercicio")
+        cursor.execute("SELECT id, nome, grupomuscular, idaparelho FROM exercicio")
         resultados = cursor.fetchall()
         cursor.close()
-        return [Exercicio(id=r[0], nome=r[1], grupomuscular=r[2], descricaoaudio=r[3], idaparelho=r[4]) for r in resultados]
+        # Converte as colunas do banco para os atributos do objeto
+        return [Exercicio(id=r[0], nome=r[1], grupo_muscular=r[2], id_aparelho=r[3]) for r in resultados]
 
-    def excluir(self, id_exercicio):
+    def excluir(self, id):
         cursor = self.conn.cursor()
-        cursor.execute("DELETE FROM exercicio WHERE id = %s", (id_exercicio,))
+        cursor.execute("DELETE FROM exercicio WHERE id = %s", (id,))
+        linhas = cursor.rowcount
         self.conn.commit()
         cursor.close()
+        return linhas > 0
