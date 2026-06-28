36da206051e370e9c3e349e921ba065493b71394
Eduardo <eduardotundis242@gmail.com>
2026-06-28 15:27:16 -0300

    Update ficha_treino.py

--- models/ficha_treino.py
+++ models/ficha_treino.py
@@ -1,22 +1,6 @@
 class FichaTreino:
-    def __init__(self, aluno_id, instrutor_id, descricao, id=None):
-        self._id = id
-        self._aluno_id = aluno_id
-        self._instrutor_id = instrutor_id
-        self._descricao = descricao
-
-    @property
-    def id(self):
-        return self._id
-
-    @property
-    def aluno_id(self):
-        return self._aluno_id
-
-    @property
-    def instrutor_id(self):
-        return self._instrutor_id
-
-    @property
-    def descricao(self):
-        return self._descricao
+    def __init__(self, id=None, idAluno=None, dataInicio=None, dataVencimento=None):
+        self.id = id
+        self.idAluno = idAluno
+        self.dataInicio = dataInicio
+        self.dataVencimento = dataVencimento
