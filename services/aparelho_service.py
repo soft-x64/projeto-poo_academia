24ce580390039f74c9c1bf9eda1a3e46bf86f33e
Eduardo <eduardotundis242@gmail.com>
2026-06-28 16:43:42 -0300

    Update aparelho_service.py

--- services/aparelho_service.py
+++ services/aparelho_service.py
@@ -1,15 +1,16 @@
-from models.aparelho import Aparelho
-
 class AparelhoService:
     def __init__(self, repository):
         self.repository = repository
 
-    def cadastrar(self, nome, grupo):
-        novo = Aparelho(nome=nome, grupoMuscular=grupo)
-        self.repository.salvar(novo)
+    def cadastrar(self, aparelho):
+        # O Service apenas encaminha o objeto para o Repository
+        self.repository.salvar(aparelho)
 
     def listar_todos(self):
         return self.repository.listar()
 
-    def excluir(self, id_aparelho):
-        self.repository.excluir(id_aparelho)
+    def excluir(self, aparelho_id):
+        if self.repository.excluir(aparelho_id):
+            print("Aparelho excluído com sucesso!")
+        else:
+            print(f"Erro: Nenhum aparelho encontrado com o ID {aparelho_id}.")
