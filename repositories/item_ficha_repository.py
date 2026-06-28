3ff63d51ebea3816718529f703b04087e6497cda
Eduardo <eduardotundis242@gmail.com>
2026-06-28 16:43:00 -0300

    Update item_ficha_repository.py

--- repositories/item_ficha_repository.py
+++ repositories/item_ficha_repository.py
@@ -8,7 +8,7 @@ class ItemFichaRepository:
     def salvar(self, item):
         cursor = self.conn.cursor()
         sql = "INSERT INTO item_ficha_treino (idficha, idexercicio, series, repeticoes, cargas) VALUES (%s, %s, %s, %s, %s)"
-        cursor.execute(sql, (item.idFicha, item.idExercicio, item.series, item.repeticoes, item.cargas))
+        cursor.execute(sql, (item.id_ficha, item.id_exercicio, item.series, item.repeticoes, item.cargas))
         self.conn.commit()
         cursor.close()
 
@@ -17,4 +17,4 @@ class ItemFichaRepository:
         cursor.execute("SELECT id, idficha, idexercicio, series, repeticoes, cargas FROM item_ficha_treino WHERE idficha = %s", (id_ficha,))
         resultados = cursor.fetchall()
         cursor.close()
-        return [ItemFichaTreino(r[0], r[1], r[2], r[3], r[4], r[5]) for r in resultados]
+        return [ItemFichaTreino(id=r[0], id_ficha=r[1], id_exercicio=r[2], series=r[3], repeticoes=r[4], cargas=r[5]) for r in resultados]
