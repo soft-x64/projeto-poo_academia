7f42dda003a7e1670ab87f6a0d61a15f6e910c0d
Eduardo <eduardotundis242@gmail.com>
2026-06-28 16:47:01 -0300

    Update exercicio_service.py

--- services/exercicio_service.py
+++ services/exercicio_service.py
@@ -1,16 +1,14 @@
-from models.avaliacao_fisica import AvaliacaoFisica
-
-class AvaliacaoFisicaService:
+class ExercicioService:
     def __init__(self, repository):
         self.repository = repository
 
-    def registrar(self, alunoId, instrutorId, peso, altura):
-        p = float(peso)
-        a = float(altura)
-        imc = p / (a * a)
-        
-        av = AvaliacaoFisica(alunoId=int(alunoId), instrutorId=int(instrutorId), peso=p, altura=a, imc=round(imc, 2))
-        self.repository.salvar(av)
+    def cadastrar(self, exercicio):
+        # Agora ele recebe apenas o objeto, como definido no seu menu
+        self.repository.salvar(exercicio)
 
-    def listar_do_aluno(self, alunoId):
-        return self.repository.buscar_por_aluno(alunoId)
+    def listar_todos(self):
+        return self.repository.listar()
+
+    def excluir(self, exercicio_id):
+        self.repository.excluir(exercicio_id)
+        print("Exercício excluído com sucesso!")
